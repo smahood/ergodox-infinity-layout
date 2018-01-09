98c44e2ee45fc02c7d7a5d2603b05e532507275a
Shaun Mahood <smahood@gmail.com>
2018-01-09 16:59:18 -0700

    Start working on QMK generation

---
+++ clj-ergodox/src/clj_ergodox/qmk_generator.clj
@@ -0,0 +1,33 @@
+(ns clj-ergodox.qmk-generator
+  (:require [clojure.edn :as edn]
+            [clojure.string]))
+
+
+
+(defn ergodox-layout [path]
+  (edn/read-string (slurp path)))
+
+
+(defn ordered-layout [keymap]
+  {:left  [:equals
+           :1 :2 :3 :4 :5 :esc
+           :backslash :q :w :e :r :t :function1
+           :tab :a :s :d :f :g
+           :lshift :z :x :c :v :b :function2
+           :lgui :backtick :function3 :function4 :function5
+           :lctrl :lalt :home :backspace :delete :end]
+   :right [:function6 :6 :7 :8 :9 :0 :minus
+           :lbrace :y :u :i :o :p :rbrace
+           :h :j :k :l :semicolon :quote
+           :function7 :n :m :comma :period :slash :rshift
+           :left :down :up :right :rgui
+           :ralt :rctrl :pageup :pagedown :enter :space]})
+
+
+(let [el (ergodox-layout "resources/ergodox-keymap.edn")
+      km (:keymap el)
+      ol (ordered-layout km)]
+  {:left  (map #(get km %)
+               (:left ol))
+   :right (map #(get km %)
+               (:right ol))})
